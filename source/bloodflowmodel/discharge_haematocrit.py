Inverse model: relative transmissibility to baseline is now included as a parameter. Bug fix in calculation hd/ht (if ht=0 --> ratio=1) (#13)

source/bloodflowmodel/discharge_haematocrit.py
@@ -37,7 +37,7 @@ class DischargeHaematocritNewtonian(DischargeHaematocrit):
         :param flownetwork: flow network object
         :type flownetwork: source.flow_network.FlowNetwork
         """
-        flownetwork.hd = np.zeros(flownetwork.nr_of_es, dtype=np.float)
+        flownetwork.hd = flownetwork.ht
 
 
 class DischargeHaematocritVitroPries1992(DischargeHaematocrit):

source/bloodflowmodel/rbc_velocity.py
@@ -69,8 +69,8 @@ class RbcVelocityFahraeus(RbcVelocity):
         hd = flownetwork.hd
         ht = flownetwork.ht
 
+        # Make sure that if ht=0, the ratio hd/ht is 1
         hd_ht_ratio = np.ones(flownetwork.nr_of_es)
-        if not 0. in ht:
-            hd_ht_ratio = hd / ht
+        hd_ht_ratio[flownetwork.ht > 0.] = flownetwork.hd[flownetwork.ht > 0.] / flownetwork.ht[flownetwork.ht > 0.]
 
         flownetwork.rbc_velocity = self._get_bulk_flow_velocity(flownetwork) * hd_ht_ratio