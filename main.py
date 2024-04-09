8c6c5bb51b0703dc34d9ecfc5bfe7cdb55b1c215
clambr <113347300+clambr@users.noreply.github.com>
2024-04-09 14:32:21 +0200

    Update text in main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-# todo: handling how different test cases can be executed based on a parameter file
-# todo: import simulation parameters from file
+# Multiple testcase options:
 
+# 1) Run test case with only the blood flow model
+# import testcases.testcase_blood_flow_model
 
-# Run test case to tune boundary pressures with the adjoint method
-# import testcases.testcase_bc_tuning
+# 2) Run test case with the blood flow considering the phase separation of red blood cells (RBCs) (iterative model)
+# import testcases.testcase_blood_flow_iterative_model
 
-# Run test case to tune diameters or transmissibilitites with the adjoint method
+# 3) Run test case with the blood flow model considering vascular distensibility
+# import testcases.testcase_distensibility
+
+# 4) Run test case to tune diameters or transmissibilitites with the inverse model
 # import testcases.testcase_inverse_problem
 
-# Run test case with only the blood flow model
+# 5) Run test case to tune boundary pressures with the inverse model
+# import testcases.testcase_bc_tuning
+
+# Import a testcase based on the options above. 
+# If not stated otherwise, testcases.testcase_blood_flow_model is executed by default
 if __name__ == "__main__":
-    import testcases.testcase_blood_flow_iterative_model
+    import testcases.testcase_blood_flow_model
